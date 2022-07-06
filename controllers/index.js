333f52ff6a4fc601490b70bc2c22bd337757b792
Jolo <jolocabrera@yahoo.com>
2022-07-06 11:43:05 -0700

    add handlebars templates for homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const apiRoute = require("./api");
+const homeRoutes = require("./home-routes");
 
+router.use("/", homeRoutes);
 router.use("/api", apiRoute);
 
 router.use((req, res) => {
